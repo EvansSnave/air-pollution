99e1b074de105fce69a1b4e33f8fcb967f0b15db
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2023-10-19 20:35:33 -0500

    change absename

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/air-pollution">
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Provider store={store}>
         <App />
       </Provider>
